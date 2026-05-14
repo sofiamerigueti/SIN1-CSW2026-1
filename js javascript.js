04453bd668382511e29ba9edb0a81db8e22e92e2
sofiamerigueti <sofiamerigueti@gmail.com>
2026-05-13 22:34:18 -0300

    Add files via upload

---
+++ js javascript.js
@@ -0,0 +1,44 @@
+var x = 10;
+if (x>5){
+    console.log("x is grater than 5");
+}else if (x<5){
+    console.log("x is equal to 5");
+} else{
+    console.log("x is equal to 5);
+}
+
+for(var i =0;i<10; i++){
+    if (i%2==0){
+     console.log(i);0,2,4,6,8
+    };
+}
+
+for (let i=0; i<10; i +=2){
+    console.log(i);0,2,4,6,8
+};
+
+for (let i=1; i < 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+y=0;
+white ( y<10) {
+    (y%2!==0) ? console.log(y): null;1,3,5,7,9
+}
+
+
+
+
