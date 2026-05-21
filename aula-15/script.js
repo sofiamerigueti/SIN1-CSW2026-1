e3f6efafd5762151206ba7a2c9261625a8141bcd
sofiamerigueti <sofiamerigueti@gmail.com>
2026-05-20 22:23:32 -0300

    Add files via upload

---
+++ aula-15/script.js
@@ -0,0 +1,31 @@
+
+document.writeln("Olá mundo!");
+document.writeln("<h2>Bem-Vindo ao Java Script</h2>");
+window.alert( "Esta é ")
+document.getElementById("title").innerHTML= "aprendendo java script com o melhor professor da UVV!!!";
+let nome=""//promp("digite seu nome:");
+
+document.getElementById("Novafrase").innerHTML="Olá",+nome+ "! Seja bem-vindo ao Jvascript!"; 
+document.getElementById("Novafrase").style.color="blue";
+document.getElementById("Novafrase").style.fontSize="80px";
+
+function= exibirMensagem(){
+let nome= document.getElementById("nome").value;
+document.getElementById("novafrase").innerHTML="Olá" +nome+ "! Seja Bem-Vindo ao javaScript";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
